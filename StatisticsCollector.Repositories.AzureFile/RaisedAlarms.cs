40fdcfa227774a4032fed53b4bee7fe764de2f72
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2014-11-08 17:16:23 +0100

    added extracted class RaisedAlarms

---
+++ StatisticsCollector.Repositories.AzureFile/RaisedAlarms.cs
@@ -0,0 +1,11 @@
+﻿using StatisticsCollector.Common;
+using StatisticsCollector.Measure;
+using System;
+using System.Collections.Generic;
+
+namespace StatisticsCollector.Repositories.AzureFile
+{
+    class RaisedAlarms: Dictionary<SensorId, AlarmInfo>
+    {
+    }
+}
