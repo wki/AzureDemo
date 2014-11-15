59b6851e40b6940496ab5f78fabf69736f0a4f99
Wolfgang Kinkeldei <wolfgang@kinkeldei.de>
2014-11-15 14:08:45 +0100

    added missing files

---
+++ AzureDemo/App_Start/Bootstrapper.cs
@@ -0,0 +1,105 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Castle.Windsor.Mvc;
+using DddSkeleton.Domain;
+using DddSkeleton.EventBus;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Mvc;
+using WebApiContrib.IoC.CastleWindsor;
+
+namespace AzureDemo
+{
+    public class Bootstrapper
+    {
+        public static IWindsorContainer Initialize()
+        {
+            var container = new WindsorContainer();
+
+            RegisterInfrastructure(container);
+            RegisterDomain(container);
+            RegisterControllers(container);
+            RegisterWebApi(container);
+
+            printRegistrations(container);
+
+            return container;
+        }
+
+        private static void printRegistrations(IWindsorContainer container)
+        {
+            Trace.TraceInformation("Registrations:");
+            foreach (var handler in container.Kernel.GetAssignableHandlers(typeof(object)))
+            {
+                foreach (var service_name in handler.ComponentModel.Services.OrderBy(s => s.Name))
+                {
+                    Trace.TraceInformation("Service: {0} Name: {1}, Implemented By {2}",
+                        service_name,
+                        handler.ComponentModel.Name,
+                        handler.ComponentModel.Implementation);
+                }
+            }
+        }
+
+        private static void RegisterInfrastructure(WindsorContainer container)
+        {
+            // return value is not of interest. Hub remains instantiated.
+            new Hub(container);
+        }
+
+        private static void RegisterDomain(WindsorContainer container)
+        {
+            var dir = new AssemblyFilter(AppDomain.CurrentDomain.RelativeSearchPath);
+
+            container.Register(Classes
+                .FromAssemblyInDirectory(dir)
+                .BasedOn(typeof(IFactory), typeof(IRepository), typeof(IService))
+                // .BasedOn<IFactory>()
+                .WithService.AllInterfaces());
+
+            //container.Register(Classes
+            //    .FromAssemblyInDirectory(dir)
+            //    .BasedOn<IRepository>()
+            //    .WithServiceAllInterfaces());
+
+            //container.Register(Classes
+            //    .FromAssemblyInDirectory(dir)
+            //    .BasedOn<IService>()
+            //    .WithServiceAllInterfaces());
+
+            container.Register(Classes
+                .FromAssemblyInDirectory(dir)
+                .BasedOn<ISubscribe<IEvent>>()
+                .WithService.AllInterfaces()
+                .Configure(c => c.Named("EventHandler:" + c.Implementation.FullName)));
+        }
+
+        private static void RegisterControllers(WindsorContainer container)
+        {
+            var factory = new WindsorControllerFactory(container.Kernel);
+            ControllerBuilder.Current.SetControllerFactory(factory);
+
+            // hier nur die innerhalb der Mvc App registrierten Typen registrieren.
+            // Der Domain kümmert sich um sich selbst
+            container.Register(Classes
+                .FromThisAssembly()
+                .BasedOn<IController>()
+                .LifestyleTransient()
+            );
+        }
+
+        private static void RegisterWebApi(WindsorContainer container)
+        {
+            GlobalConfiguration.Configuration.DependencyResolver = new WindsorResolver(container);
+
+            container.Register(Classes
+                .FromThisAssembly()
+                .BasedOn<ApiController>()
+                .LifestylePerWebRequest() // we must ensure to call a service only once which is a usual case.
+                // .LifestyleScoped()  // does not work. dunno why
+            );
+        }
+    }
+}
---
+++ AzureDemo/Controllers/Api/MeasureController.cs
@@ -0,0 +1,36 @@
+﻿using StatisticsCollector.App;
+using System;
+using System.Diagnostics;
+using System.Web.Http;
+
+namespace AzureDemo.Controllers.Api
+{
+    [RoutePrefix("api/measure")]
+    public class MeasureController : ApiController
+    {
+        public IMeasureService MeasureService { get; set; }
+
+        public MeasureController(IMeasureService measureService)
+        {
+            MeasureService = measureService;
+        }
+
+        [Route("{location}/{part}/{measure}/provide")]
+        [HttpPost]
+        public IHttpActionResult provideResult(
+            string location, string part, string measure, [FromBody] PostResult postResult)
+        {
+            var sensorName = String.Join("/", location, part, measure);
+            Trace.TraceInformation("Sensor {0} provides result {1}", sensorName, postResult.Result);
+            MeasureService.ProvideResult(sensorName, postResult.Result);
+
+            return Ok();
+        }
+    }
+
+    // just a simple helper to get things from a json input.
+    public class PostResult
+    {
+        public int Result { get; set; }
+    }
+}
